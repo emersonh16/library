bf25c3592b3d2046db8f5dbe47fec3c2bd29183b
unknown <emersonh16@gmail.com>
2022-08-22 12:12:39 -0400

    beginning js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,42 @@
+let myLibrary = [{title:"Jurassic Park", author:"Michael Criton", pages: 458, read: true}, {title:"Harry Potter",author:"JKR", pages:315, read: true}];
 
+
+function bookConstructor(title,author,pages,read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+
+        var readStatus = "empty" ;
+
+            if(read == true){
+                readStatus = "already read."
+                }
+            else {
+                readStatus = "not read yet."
+            }
+
+         return title + " by " + author + ", " + pages + " pages, " + readStatus
+    }
+}
+
+function addBookToLibrary(bookConstructor) {
+    var newBook = bookConstructor;
+
+    myLibrary.push(newBook)
+    }
+
+
+function displayLibrary () {
+
+    let contents = document.getElementById("bookList").innerHTML
+
+    for (let i=0; i < myLibrary.length; i++) {
+        contents += myLibrary[i]
+    }
+
+    return contents
+}
+
+document.addEventListener("load", displayLibrary);
